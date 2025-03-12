874e3fb17f516ebaa6ca8d49c45b8a40381cfb9a
maniebra <mani.ebra@gmail.com>
2025-03-12 18:39:19 +0330

    implemented basic onclick handlers

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,14 +7,38 @@ binding.submitItem.addEventListener("click", () => {
   alert("clicked");
 });
 
-const formulaElement = document
-  .querySelectorAll("formula")
-  .forEach((formulaElement) => {
-    document.querySelectorAll("input[id]").forEach((input) => {
-      input.addEventListener("change", () => {
-        const result = evaluateFormulas(formulaElement, binding);
+binding.changeImage.addEventListener("click", () => {
+  binding.imagePicker.click();
+});
 
-        console.log("Result:", result);
+binding.imagePicker.addEventListener("click", () => {
+  const file = event.target.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      const imageElement = document.querySelector(".detail-form .image");
+      imageElement.style.backgroundImage = `linear-gradient(
+              to bottom,
+              rgba(0, 0, 0, 0),
+              rgba(0, 0, 0, 0.9) 86%
+          ), url(${e.target.result})`;
+    };
+    reader.readAsDataURL(file);
+  }
+});
+
+document.querySelectorAll("input[id]").forEach((input) => {
+  const formulaElement = document
+    .querySelectorAll("formula")
+    .forEach((formulaElement) => {
+      input.addEventListener("change", () => {
+        binding.calculations.innerHTML = "";
+        const result = evaluateFormulas(formulaElement, binding);
+        if (result) {
+          const resultElement = document.createElement("div");
+          resultElement.textContent = `${formulaElement.dataset.label} : ${result}`;
+          binding.calculations.appendChild(resultElement);
+        }
       });
     });
-  });
+});
